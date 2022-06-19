62b8cf5d3beb4d0d0a40a60ae3f5059b5841c331
Váradi Sándor <varadisandor011@gmail.com>
2022-06-19 08:44:41 +0200

    not the First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+
 public class Main
 {
     public static void main(String[] args)
@@ -7,10 +10,14 @@ public class Main
             System.out.println("Hello world!");
         }
 
+
+
         for (int i = 0; i < 1; i++)
         {
             System.out.println("Hello world!");
         }
+
+        ArrayList<Integer> myList = new ArrayList<>();
 
         Person person = new Person();
 
@@ -19,6 +26,55 @@ public class Main
 
         person.setName("Nagy Ervin");
 
-        System.out.printf("Sziaaaaaa %s", person.getName());
+        System.out.printf("Sziaaaaaa %s\n", person.getName());
+
+
+        StringBuilder sb = new StringBuilder();
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000;i++)
+        {
+            sb.append(i);
+        }
+        sb.append("\n");
+
+        long finish = System.currentTimeMillis();
+
+        System.out.printf("String Bulder Time: %f\n", (double)(finish - start) / 1000);
+
+        String st = "";
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100000;i++)
+        {
+            st += i;
+        }
+        st += '\n';
+
+        finish = System.currentTimeMillis();
+
+        System.out.printf("String Concat. Time: %f\n", (double)(finish - start) / 1000);
+
+
+        int count = 0;
+
+        String[][] things = {
+                                {"scarf", "gloves", "hat"},
+                                {"shoes", "shirt", "pants"}
+                            };
+
+        for (int r = 0; r < things.length; r++)
+        {
+            for (int c = 0; c < things[r].length - 1; c++)
+            {
+                if (things[r][c].indexOf("a") >= 0)
+                {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
